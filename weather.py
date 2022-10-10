0dbeaf8cd10e91c857cc322a25acbf718615e056
zaol <luirol@mail.ru>
2022-10-10 13:15:17 +0300

    v2

--- weather.py
+++ weather.py
@@ -1,11 +1,11 @@
 from urllib import response
 import requests
 
-list_params = {'TqnM':'', 'lang':'ru'}
+getParams = {'TqnM':'', 'lang':'ru'}
 areas = ['svo', 'London', 'Череповец']
 for area in areas:
     url = 'HTTP://wttr.in/{}'
-    url = url.format(area) # svo London Череповец
-    response = requests.get(url, params = list_params)
+    url = url.format(area)
+    response = requests.get(url, params = getParams)
     response.raise_for_status()
     print(response.text)
